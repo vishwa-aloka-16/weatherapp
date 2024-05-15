e2cf52fe47c709272e2d2220f31c8b7a918ea2ed
vishwa-aloka-16 <vishwaaloka16@gmail.com>
2024-05-15 08:17:55 +0530

    Funtionality Update
    Completed Back End

--- script.js
+++ script.js
@@ -27,9 +27,28 @@ let city = [
     [1234808,"Monaragala",6.871400,81.348701],
     [1246007,"Gampaha",7.089700,79.992500],
 ];
+//HTML ELEMENTS
+const weatherDay = document.getElementsByClassName("weatherDay");
+const weatherTime = document.getElementsByClassName("weatherTime");
+const watherDis = document.getElementsByClassName("watherDis");
+const weatherDayPartContainer = document.getElementsByClassName("weatherDayPartContainer");
+const cityName = document.getElementById('cityName');
+const cityTemp = document.getElementById('cityTemp');
+const cityvalue = document.getElementById("city");
+const watherInfo = document.getElementsByClassName("weatherInfo");
 
 
-let userCity = "Jaffna";
+
+cityvalue.addEventListener('click',function () {
+    let userCity = cityvalue.value;
+    cityName.innerHTML = userCity;
+
+
+
+
+
+
+//City Id Finder
 let cityID =0;
 let k=0
 city.forEach(e => {
@@ -41,18 +60,104 @@ city.forEach(e => {
     });
     k++;
 });
-// console.log(userCity,"City Id is",cityID);
-let tempData = document.getElementById('temp');
-let cityData = document.getElementById('city');
-let temp = 0; 
+
+
+const today = new Date();
+const year = today.getFullYear();
+let month = today.getMonth() + 1;  1
+month = month < 10 ? '0' + month : month; 
+const day = today.getDate();
+
+const TodayDate = `${year}-${month}-${day}`;
+console.log(TodayDate); 
+
+
+//Functions 
+
+function dayFinder(today,day) {
+    if( today == day){
+        return "Today";
+    }else if(parseInt(day.slice(8, 10)) == parseInt(today.slice(8, 10))-1){
+        return "Yesterday";
+    }else if(parseInt(day.slice(8, 10)) == (parseInt(today.slice(8, 10))+1)){
+        return "Tommorow";
+    }
+}
+function timeFinder(time){
+    let hrs = time.split(":")[0];
+
+    if (0<=hrs && hrs<=9 ) {
+        return "Morning"
+    } else if(9<hrs && hrs<=14){
+        return "Noon"
+    }else if(14<hrs && hrs<=18){
+        return "Evening"
+    }else if(18<hrs && hrs<=24){
+        return "Night"
+    }
+}
+
+function backgroundColorSelector(weather) {
+    if (weather == "Clouds") {
+        return " #cccccc"
+    } else if (weather == "Rain") {
+        return "#c7e9ff"
+    }else if (weather == "Clear") {
+        return "#ffea80"
+    }else if (weather == "Thunderstorm") {
+        return "#999893"
+    }else if (weather == "Drizzle") {
+        return "#d4fbfc"
+    }else if (weather == "Mist") {
+        return "#f5fafa"
+    }
+}
+
+//API RESPONCE
 fetch('http://api.openweathermap.org/data/2.5/forecast?id='+cityID+'&appid=a901d4f6fafbf6ac750033d06f3acdbb')
   .then(response => {
     return response.json();
   })
   .then(data => {
-    console.log(data)
-    temp = +(data.list[0].main.temp-273).toFixed(2)+"C";
-    tempData.innerHTML = temp;
-    cityData.innerHTML = data.city.name;
+    console.log(data);
+//API WORK
+
+    let weatherDisplayOne = data.list[0].dt_txt.split(" ");
+    let weatherDisplayTwo = data.list[3].dt_txt.split(" ");
+    let weatherDisplayThree = data.list[5].dt_txt.split(" ");
+    weatherDay[0].innerHTML= dayFinder(TodayDate,weatherDisplayOne[0]);
+    weatherDay[1].innerHTML= dayFinder(TodayDate,weatherDisplayTwo[0]);
+    weatherDay[2].innerHTML= dayFinder(TodayDate,weatherDisplayThree[0]);
+    weatherTime[0].innerHTML= timeFinder(weatherDisplayOne[1]);
+    weatherTime[1].innerHTML= timeFinder(weatherDisplayTwo[1]);
+    weatherTime[2].innerHTML= timeFinder(weatherDisplayThree[1]);
+
+    watherDis[0].innerHTML= data.list[0].weather[0].description;
+    watherDis[1].innerHTML= data.list[3].weather[0].description;
+    watherDis[2].innerHTML= data.list[5].weather[0].description;
+
+    weatherDayPartContainer[0].style.backgroundColor = backgroundColorSelector(data.list[0].weather[0].main);
+    weatherDayPartContainer[1].style.backgroundColor = backgroundColorSelector(data.list[3].weather[0].main);
+    weatherDayPartContainer[2].style.backgroundColor = backgroundColorSelector(data.list[5].weather[0].main);
+    cityTemp.innerHTML= (data.list[0].main.temp-273).toFixed(0)+"°C";
+
+    // watherInfo[0].innerHTML.innerHTML = 
+    watherInfo[0].innerHTML= "Temperature(Feels Like):&nbsp;&nbsp;"+ data.list[0].main.feels_like +"K";
+    watherInfo[1].innerHTML= "Max Temperature:&nbsp;&nbsp;"+ data.list[0].main.temp_max+"K";
+    watherInfo[2].innerHTML= "Min Temperature:&nbsp;&nbsp;"+ data.list[0].main.temp_min +"K";
+    watherInfo[3].innerHTML= "Humidity:&nbsp;&nbsp;"+ data.list[0].main.humidity ;
+    watherInfo[4].innerHTML= "Pressure:&nbsp;&nbsp;"+ data.list[0].main.pressure;
+    watherInfo[5].innerHTML= "Visibility:&nbsp;&nbsp;"+ data.list[0].wind.speed ;
+    watherInfo[6].innerHTML= "Wind:&nbsp;&nbsp;"+ data.list[0].visibility;
+    watherInfo[7].innerHTML= "Rain:&nbsp;&nbsp;"+ data.list[0].rain["3h"] ;
+
+
+
+
+
+
+
+
 });
 
+});
